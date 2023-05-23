9d2d2dabab6c60e6e9e9f4c8c4f8175f1750dc6c
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-05-22 23:05:36 -0300

    feat : Serve directory admin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// TODO implement server
+import { config } from "dotenv";
+import * as express from "express";
+import { dirname } from "./dirname";
+import { resolve } from "path";
+
+config();
+
+const port = parseInt(process.env.PORT || "8080");
+
+const app = express.default();
+
+app.use("/admin", express.static(resolve(dirname + "/../admin")));
+
+app.listen(port, () => {
+  console.log(`Server started at port ${ port }`);
+});
