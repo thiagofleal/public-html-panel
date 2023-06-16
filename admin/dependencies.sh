989d5c224ca9ce5c8a4ad2cc9307f6e8a53e07a3
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-06-15 22:30:58 -0300

    build (ugdm) : Update dependencies.sh

--- admin/dependencies.sh
+++ admin/dependencies.sh
@@ -5,7 +5,7 @@ git clone https://github.com/thiagofleal/small-reactive small-reactive
 cd small-reactive
 git fetch https://github.com/thiagofleal/small-reactive && git checkout dev
 git pull https://github.com/thiagofleal/small-reactive dev
-git checkout fe05dc897a6595e71ffd77bf0653a155c073ff3a
+git checkout 3b192ec797225f02b2b123c6da7f91b81b6999dc
 sh dependencies.sh
 cd ..
 cd vendor/
